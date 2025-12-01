9e73dde62025e52f7c3d8d9a4efb07f6900c3f71
Matt Bargenquast <matt.bargenquast@gmail.com>
2025-12-01 20:13:11 +1000

    Day 1 added

--- day01/main.go
+++ day01/main.go
@@ -3,26 +3,73 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/mrbarge/aoc2025-golang/helper"
 )
 
-func problem(lines []string, partTwo bool) (r float64, err error) {
-	return 0, nil
+// euclidean mod
+func mod(n, m int) int {
+	return ((n % m) + m) % m
+}
+
+func zerocross(pos int, spin int) int {
+	if spin >= 0 {
+		return (pos + spin) / 100
+	} else {
+		pspin := -spin
+		if pos == 0 {
+			return pspin / 100
+		} else if pspin >= pos {
+			return (pspin-pos)/100 + 1
+		} else {
+			return 0
+		}
+	}
+}
+
+func rotate(pos int, cmd string) (r int, zeroCrosses int, err error) {
+	spin, _ := strconv.Atoi(cmd[1:])
+	if cmd[0] == 'L' {
+		spin = -spin
+	}
+	zeroCrosses = zerocross(pos, spin)
+	return mod(pos+spin, 100), zeroCrosses, nil
+}
+
+func partOne(lines []string) (r int, err error) {
+	pos := 50
+	for _, line := range lines {
+		pos, _, err = rotate(pos, line)
+		if pos == 0 {
+			r++
+		}
+	}
+	return r, nil
+}
+
+func partTwo(lines []string) (r int, err error) {
+	pos := 50
+	zc := 0
+	for _, line := range lines {
+		pos, zc, _ = rotate(pos, line)
+		r += zc
+	}
+	return r, nil
 }
 
 func main() {
-	fh, _ := os.Open("test.txt")
+	fh, _ := os.Open("input.txt")
 	lines, err := helper.ReadLines(fh, true)
 	if err != nil {
 		fmt.Printf("Unable to read input: %v\n", err)
 		return
 	}
 
-	ans, err := problem(lines, false)
-	fmt.Printf("Part one: %f\n", ans)
+	ans, err := partOne(lines)
+	fmt.Printf("Part one: %d\n", ans)
 
-	ans, err = problem(lines, true)
-	fmt.Printf("Part two: %f\n", ans)
+	ans, err = partTwo(lines)
+	fmt.Printf("Part two: %d\n", ans)
 
 }
--- day01/main_test.go
+++ day01/main_test.go
@@ -2,7 +2,25 @@ package main
 
 import (
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("day01", func() {
+
+	DescribeTable("testing rotations",
+		func(startingPos int, operation string, endingPos int, zeroCrosses int) {
+			result, zerocross, err := rotate(startingPos, operation)
+			Expect(result).To(Equal(endingPos))
+			Expect(zerocross).To(Equal(zeroCrosses))
+			Expect(err).To(BeNil())
+		},
+		Entry("when rotating right but not past 99", 10, "R10", 20, 0),
+		Entry("when rotating left but not past 0", 10, "L5", 5, 0),
+		Entry("when rotating right past 99", 50, "R60", 10, 1),
+		Entry("when rotating left past 0", 50, "L60", 90, 1),
+		Entry("when rotating right past 0 many times", 50, "R400", 50, 4),
+		Entry("when rotating left past 0 many times", 50, "L400", 50, 4),
+		Entry("when rotating left past 0 many times", 50, "L150", 0, 2),
+		Entry("when rotating right past 0 many times", 50, "R150", 0, 2),
+	)
 })
